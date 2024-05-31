25f70f96c6835f30eeece5a906f478292b5d5445
Tolga Boran Alper <60055681+tolgabalper@users.noreply.github.com>
2024-05-31 21:32:45 +0000

    Completed the saved thing

--- calc.py
+++ calc.py
@@ -8,19 +8,43 @@ Welcome = "Welcome to PyCalculator by Boran A."
 Options =  "SUM // MINUS // MULTIPLICATION (MULT) // DIVISION // PERMUTATION // COMBINATION // POWER"
 
 print(Welcome + "\n")
-print(Options + "\n")
 
-def Calculate() :
-    while True:
-        if saved != [] :
-            print(saved)
-        operation = input("Please select the operation you wanna use : ").lower()
+def savedselectorfunc() :
+    print("Saved results are :", saved)
+    isUserWantUseSaved = input("Do you wanna use any of the saved results : (y / N) ")
+    if isUserWantUseSaved == "y" :
+        isUserWantUseOne = input("Do you wanna just use one of them : (y / N)")
+        if isUserWantUseOne == "y" :
+            print("Saved results are :", saved)
+            whichOne = input("Which one you wanna use : ").replace(',','.')
+            x = float(whichOne)
+            y = input("Enter the second number: ").replace(',', '.')
+            y = float(y)                    
+        else :
+            print("Saved results are :", saved)
+            whichOne = input("Which one you wanna use for first number : ").replace(',','.')
+            whichTwo = input("Which one you wanna use for two number : ").replace(',','.')
+            x = float(whichOne)
+            y = float(whichTwo)
+    else :
         x = input("Enter the first number: ").replace(',', '.')
         x = float(x)
         y = input("Enter the second number: ").replace(',', '.')
         y = float(y)
+    return x, y
 
+def Calculate() :
+    while True:
+        print(Options + "\n")
+        operation = input("Please select the operation you wanna use : ").lower()
         result = ""
+        if saved != [] :
+            x, y = savedselectorfunc()
+        else:
+            x = input("Enter the first number: ").replace(',', '.')
+            x = float(x)
+            y = input("Enter the second number: ").replace(',', '.')
+            y = float(y)
 
         if operation == "sum" :
             result = math.fsum([x, y])
@@ -44,7 +68,8 @@ def Calculate() :
         elif operation == "power" :
             result = math.pow(x, y)
         else :
-            print("please select according to the list : ", Options)
+            print("please select according to the list")
+            break
     
         print(result)
         saveopt = input("Do you wanna save the result : (y / n) ").lower()
@@ -53,18 +78,27 @@ def Calculate() :
             print("Result saved.")
             savecomp = input("Do you wanna save the result to your computer : (y / N) ").lower()
             if savecomp == "y" :
-                savepath = input("Enter the path where you want to save")
+                savepath = input("Enter the path where you want to save : ")
                 saveformat = input("Which format do you wanna save results : (txt, csv, pdf) ")
                 if saveformat == "pdf" :
                     c = canvas.Canvas(savepath + "/result.pdf").drawString(100, 750, "Result: " + str(result))
                     c.save()
+                    isWanting = input("Do you wanna do another calculation : (y / n)")
+                    if isWanting != "y" :
+                        break                    
                 if saveformat == "txt" :
                     with open(savepath + "/result.txt", 'x') as f :
                         f.write("Results" + "\n" + str(result))
+                    isWanting = input("Do you wanna do another calculation : (y / n)")
+                    if isWanting != "y" :
+                        break                        
                 if saveformat == "csv" :
                     with open(savepath + "/result.csv", 'x', newline='') as f :
                         csv.writer(f).writerow("Results")
                         csv.writer(f).writerow([result])
+                    isWanting = input("Do you wanna do another calculation : (y / n)")
+                    if isWanting != "y" :
+                        break                        
             else :
                     isWanting = input("Do you wanna do another calculation : (y / n)")
                     if isWanting != "y" :
